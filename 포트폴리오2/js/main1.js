802f6ae885205a836a4d7550165e3f4530768fa1
umheabin <87254253+umheabin@users.noreply.github.com>
2022-06-30 09:43:48 +0900

    Add files via upload

---
+++ 포트폴리오2/js/main1.js
@@ -0,0 +1,39 @@
+const btn = document.querySelector("#button1");
+const tab = document.querySelector(".tab-1")
+const btn1 = document.querySelector(".button2");
+const textbox1 = document.querySelector("#text1");
+const text1 = document.querySelector("#text");
+
+
+btn.addEventListener('click',() => {
+
+    if(tab.style.display === "block") {
+        tab.style.display = "none";
+    } else {
+        tab.style.display = "block";
+    }
+
+});
+
+btn1.addEventListener('click',() => {
+    window.open("index2.html", 'child', 'width=1000, height=800', true);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
